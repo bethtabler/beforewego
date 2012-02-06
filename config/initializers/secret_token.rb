81f430d151a75676ec214ed55fc5945927ef06b0
Beth Tabler <beth@beforewego.net>
2012-02-05 23:35:13 -0800

    intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beforewego::Application.config.secret_token = '3f9dad6ed5ec6c59e260fe6625cebb465d0aa37fd93167d794d8ac4158dc8129153dc14d8a21934029e0c62412ee033079967ab51160d8e0292122976541cf44'
